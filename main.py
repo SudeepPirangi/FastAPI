d89a00acf472ebacd15db349b541ad9fd33549a4
Sudeep Pirangi <sudeep_edu@yahoo.com>
2026-03-12 20:53:28 +0530

    Adds basic endpoints

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import Optional
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -8,13 +10,35 @@ def index():
     """
     Basic function in the app
     """
-    return {"data": {"name": "Hello World!"}}
+    return {"data": "Blog list"}
 
 
-@app.get("/about")
-def about():
-    """About response"""
-    return {"data": {"name": "About Page"}}
+@app.get("/blogs")
+def get_all_blogs(limit: int = 10, published: bool = True):
+    """
+    Get List of all blogs
+    """
+    return {"data": f"List of {limit} blogs - {published}"}
+
+
+class Blog(BaseModel):
+    """Blog model"""
+
+    title: str
+    body: str
+    published: Optional[bool]
+
+
+@app.post("/blog")
+def create_blog(request: Blog):
+    """Create a Blog"""
+    return request
+
+
+@app.get("/blog/{blog_id}")
+def show_blog(blog_id: int, published: bool = True):
+    """Individual Blog"""
+    return {"data": f"Blog #{blog_id}, published - {published}"}
 
 
 if __name__ == "__main__":
